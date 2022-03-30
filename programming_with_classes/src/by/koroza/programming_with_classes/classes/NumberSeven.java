38f70bcd0d55c85fc31f49f67e819b7817d07964
Alexey <koroza.alexey99@gmail.com>
2022-03-30 19:28:11 +0200

    *

---
+++ programming_with_classes/src/by/koroza/programming_with_classes/classes/NumberSeven.java
@@ -0,0 +1,15 @@
+/* Описать класс, представляющий треугольник. 
+ * Предусмотреть методы для создания объектов, 
+ * вычисления площади, периметра и точки пересечения медиан. 
+ */
+
+package by.koroza.programming_with_classes.classes;
+
+public class NumberSeven {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ programming_with_classes/src/by/koroza/programming_with_classes/classes/NumberTwo.java
@@ -0,0 +1,16 @@
+/* Создйте класс Test2 двумя переменными. 
+ * Добавьте конструктор с входными параметрами. 
+ * Добавьте конструктор, инициализирующий члены класса по умолчанию. 
+ * Добавьте set- и get- методы для полей экземпляра класса. 
+ */
+
+package by.koroza.programming_with_classes.classes;
+
+public class NumberTwo {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
