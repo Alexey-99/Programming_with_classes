2601b2139f78046eb1ae282a06a63c93f8243d46
Alexey <koroza.alexey99@gmail.com>
2022-04-20 11:49:31 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/classes/numbertwo/Test2.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/classes/numbertwo/Test2.java
@@ -1,5 +1,32 @@
 package by.koroza.programming_with_classes.classes.numbertwo;
 
 public class Test2 {
+	private int numberOne;
+	private int numberTwo;
 
+	public Test2(int numberOne, int numberTwo) {
+		this.numberOne = numberOne;
+		this.numberTwo = numberTwo;
+	}
+
+	public Test2() {
+		this.numberOne = 0;
+		this.numberTwo = 0;
+	}
+
+	public int getNumberOne() {
+		return numberOne;
+	}
+
+	public void setNumberOne(int numberOne) {
+		this.numberOne = numberOne;
+	}
+
+	public int getNumberTwo() {
+		return numberTwo;
+	}
+
+	public void setNumberTwo(int numberTwo) {
+		this.numberTwo = numberTwo;
+	}
 }
