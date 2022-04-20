7428f6b8b0e57b9ebc784226441f593f49d1e1a5
Alexey <koroza.alexey99@gmail.com>
2022-04-20 13:38:38 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/classes/numberthree/NumberThree.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/classes/numberthree/NumberThree.java
@@ -9,7 +9,10 @@ package by.koroza.programming_with_classes.classes.numberthree;
 public class NumberThree {
 
 	public static void main(String[] args) {
-		
+		Student[] students = { new Student("Koroza", "AM", 1), new Student("Sobol", "SL", 2),
+				new Student("Abramson", "AN", 1), new Student("Howard", "HD", 1), new Student("Jacobson", "JN", 2),
+				new Student("Keat", "KT", 2), new Student("Laird", "LD", 1), new Student("Macadam", "MM", 2),
+				new Student("MacDonald", "MD", 1), new Student("Miller", "MR", 2) };
 	}
 
 }
