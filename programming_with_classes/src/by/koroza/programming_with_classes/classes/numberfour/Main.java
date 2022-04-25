a516881c72e67225e5e07269eeedf668b5e63324
Alexey <koroza.alexey99@gmail.com>
2022-04-25 19:30:59 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/classes/numberfour/Main.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/classes/numberfour/Main.java
@@ -1,12 +1,12 @@
-/* Создайте класс Train, содержащий поля: + 
- * - название пункта назначения,  +
- * - номер поезда, +
- * - время отправления. +
- * Создайте данные в массив из пяти элементов типа Train, + 
- * добавьте возможность сортировки элементов массива по номерам поездов. +
+/* Создайте класс Train, содержащий поля:  
+ * - название пункта назначения,  
+ * - номер поезда, 
+ * - время отправления. 
+ * Создайте данные в массив из пяти элементов типа Train, 
+ * добавьте возможность сортировки элементов массива по номерам поездов. 
  * Добавьте возможность вывода информации о поезде, номер которого введен пользователем. 
- * Добавьте возможность сортировки массив по пункту назначения, +
- * причем поезда с одинаковыми пунктами назначения должны быть упорядочены по времени отправления. +
+ * Добавьте возможность сортировки массив по пункту назначения, 
+ * причем поезда с одинаковыми пунктами назначения должны быть упорядочены по времени отправления. 
  */
 
 package by.koroza.programming_with_classes.classes.numberfour;
@@ -20,6 +20,9 @@ public class Main {
 	final static String OPERATION_THREE = "3 - Sorting timetable by destination";
 	final static String OPERATION_FOUR = "4 - Display information about the train.";
 	final static String OPERATION_ZERO = "0 - Exit";
+	final static String INPUT_NUMBER_TRAIN = "Please, input interesting number train";
+	final static String INFOMATION_ONE_MORE_TRAIN = "Do you want to know information about one nore train? Input, please, Yes - 1 or No - 0";
+	final static String SELECT_NEW_OPERATION = "Do you want to select new operation? Input, please, Yes - 1 or No - 0.";
 
 	public static void main(String[] args) {
 		Train[] trains = { new Train("Kiev", 1, "05:20"), new Train("Oslo", 3, "20:30"), new Train("Brest", 2, "15:35"),
@@ -41,14 +44,21 @@ public class Main {
 				Train.printSortingTimetableByDestination(trains);
 				break;
 			case "4":
-				
-				Train.printOriginalTimetable(trains);
-				String numberTrain = inputTrainNumber(trains);
-				Train.printInformationAboutSelectedTrain(trains, numberTrain);
+				String answerContinueOperation = "";
+				do {
+					Train.printOriginalTimetable(trains);
+					String numberTrain = inputTrainNumber(trains);
+					Train.printInformationAboutSelectedTrain(trains, numberTrain);
+					answerContinueOperation = inputWantKnowInformationAboutOneMoreTrain(trains);
+				} while (answerContinueOperation.equals("1"));
 				break;
 			}
-			
-			numberOperation = inputNumberOperation(trains);
+			String answerNewOperation = inputWantContinue();
+			if (answerNewOperation.equals("1")) {
+				numberOperation = inputNumberOperation(trains);
+			} else {
+				numberOperation = answerNewOperation;
+			}
 		}
 	}
 
@@ -71,16 +81,42 @@ public class Main {
 	}
 
 	private static String inputTrainNumber(Train[] trains) {
+		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 		boolean flagCorrect;
 		String numberTrain = "";
 		do {
 			flagCorrect = false;
-			System.out.println("Please, input interesting number train");
+			System.out.println(INPUT_NUMBER_TRAIN);
 			numberTrain = scan.nextLine();
 			flagCorrect = Validation.validationOnCorrectInputNumberTrain(numberTrain, trains);
 		} while (flagCorrect == false);
-		scan.close();
 		return numberTrain;
+	}
+
+	private static String inputWantKnowInformationAboutOneMoreTrain(Train[] trains) {
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		boolean isCorrectAnswer;
+		String answer;
+		do {
+			System.out.println(INFOMATION_ONE_MORE_TRAIN);
+			answer = scan.nextLine();
+			isCorrectAnswer = Validation.validationAnswerOneOrZero(answer);
+		} while (isCorrectAnswer == false);
+		return answer;
+	}
+
+	private static String inputWantContinue() {
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		boolean isCorrectAnswer;
+		String answer;
+		do {
+			System.out.println(SELECT_NEW_OPERATION);
+			answer = scan.nextLine();
+			isCorrectAnswer = Validation.validationAnswerOneOrZero(answer);
+		} while (isCorrectAnswer == false);
+		return answer;
 	}
 }
--- programming_with_classes/src/by/koroza/programming_with_classes/classes/numberfour/Validation.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/classes/numberfour/Validation.java
@@ -4,32 +4,49 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validation {
+	final static String ENTERED_OPERATION_NUMBER_INCORRACTLY = "You entered the operation number incorrectly.";
+	final static String ENTERED_TRAIN_NUMBER_INCORRACTLY = "You entered the train number incorrectly.";
+	final static String ENTERED_ANSWER_NUMBER_INCORRACTLY = "You entered the answer number incorrectly.";
+	final static String DOES_NOT_HAVE_TRAIN_WITH_NUMBER = "Doesn't have train with this number '";
+	final static String REG_EXR_ON_DIGITAL_INPUT = "\\d*";
+	final static String REG_EXR_ENTERED_OPERATION_NUMBER_INCORRACTLY = "[0|1|2|3|4]";
+	final static String REG_EXR_ENTERED_ANSWER_NUMBER_INCORRACTLY = "[0|1]";
 
 	public static boolean validationOnCorrectInputNumberTrain(String numberTrain, Train[] trains) {
 		boolean flagCorrectInput = validationOnDigitalInput(numberTrain);
 		if (flagCorrectInput == true) {
 			flagCorrectInput = validationOnPresenceTrainWithNumber(numberTrain, trains);
 		}
-		if (flagCorrectInput == false) {
-			System.out.println("You entered the operation number incorrectly.");
-		}
 		return flagCorrectInput;
 	}
 
 	public static boolean validationOnCorrectInputNumberOperaion(String numberOperation) {
-		Pattern p = Pattern.compile("[0|1|2|3|4]");
+		Pattern p = Pattern.compile(REG_EXR_ENTERED_OPERATION_NUMBER_INCORRACTLY);
 		Matcher m = p.matcher(numberOperation);
 		boolean flagCorrectInput = m.matches();
 		if (flagCorrectInput == false) {
-			System.out.println("You entered the operation number incorrectly.");
+			System.out.println(ENTERED_OPERATION_NUMBER_INCORRACTLY);
 		}
 		return flagCorrectInput;
 	}
 
+	public static boolean validationAnswerOneOrZero(String answer) {
+		Pattern p = Pattern.compile("[0|1]");
+		Matcher m = p.matcher(answer);
+		boolean isCorrectAnswer = m.matches();
+		if (isCorrectAnswer == false) {
+			System.out.println(ENTERED_ANSWER_NUMBER_INCORRACTLY);
+		}
+		return isCorrectAnswer;
+	}
+
 	private static boolean validationOnDigitalInput(String numberTrain) {
-		Pattern p = Pattern.compile("\\d*");
+		Pattern p = Pattern.compile(REG_EXR_ON_DIGITAL_INPUT);
 		Matcher m = p.matcher(numberTrain);
 		boolean flagCorrectInput = m.matches();
+		if (flagCorrectInput == false) {
+			System.out.println(ENTERED_TRAIN_NUMBER_INCORRACTLY);
+		}
 		return flagCorrectInput;
 	}
 
@@ -41,6 +58,10 @@ public class Validation {
 				flagCorrectInput = true;
 			}
 		}
+		if (flagCorrectInput == false) {
+			System.out.println(DOES_NOT_HAVE_TRAIN_WITH_NUMBER + numberTrain + "'.");
+		}
 		return flagCorrectInput;
 	}
+
 }
