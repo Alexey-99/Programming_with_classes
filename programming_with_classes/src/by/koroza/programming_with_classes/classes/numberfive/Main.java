55fd3ce08f945a48c34ba1d5737adf6a84c0e360
Alexey <koroza.alexey99@gmail.com>
2022-04-27 14:55:23 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/classes/numberfive/Main.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/classes/numberfive/Main.java
@@ -8,9 +8,33 @@
 
 package by.koroza.programming_with_classes.classes.numberfive;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		String[] array = { "Adfbdfb", "zdfbdfcb", "dfbdbvdxcb", "zvzcvxcvxcbx" };
+		sort1(array);
+		sort2(array);
 	}
+
+	private static void sort1(String[] array) {
+		Arrays.sort(array);
+		System.out.println(Arrays.toString(array));
+	}
+
+	private static void sort2(String[] array) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i; j < array.length; j++) {
+				int compare = array[i].compareTo(array[j]);
+				if (compare > 0) {
+					String tmp = array[i];
+					array[i] = array[j];
+					array[j] = tmp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(array));
+	}
+
 }
