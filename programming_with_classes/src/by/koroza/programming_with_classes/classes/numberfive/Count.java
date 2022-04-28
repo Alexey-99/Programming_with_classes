b87408a2942ab4757026cb0e86cfa7e37d2363e3
Alexey <koroza.alexey99@gmail.com>
2022-04-28 14:07:36 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/classes/numberfive/Count.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/classes/numberfive/Count.java
@@ -2,6 +2,8 @@ package by.koroza.programming_with_classes.classes.numberfive;
 
 public class Count {
 	private int value;
+	private int minValue;
+	private int maxValue;
 
 	public Count() {
 		this.value = 0;
@@ -18,6 +20,35 @@ public class Count {
 	public void setValue(int value) {
 		this.value = value;
 	}
-	
-	
+
+	public int getMinValue() {
+		return minValue;
+	}
+
+	public void setMinValue(int minValue) {
+		this.minValue = minValue;
+	}
+
+	public int getMaxValue() {
+		return maxValue;
+	}
+
+	public void setMaxValue(int maxValue) {
+		this.maxValue = maxValue;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 31;
+		int prime = 1;
+		result = result * prime + value;
+		result = result * prime + minValue;
+		result = result * prime + maxValue;
+		return result;
+	}
+
+	public boolean equals(Object object) {
+		
+		return true;
+	}
 }
