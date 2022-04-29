431706920df2bd19eb0fee879b2c3e267851eb61
Alexey <koroza.alexey99@gmail.com>
2022-04-29 15:54:40 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/classes/numbersix/Time.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/classes/numbersix/Time.java
@@ -2,4 +2,36 @@ package by.koroza.programming_with_classes.classes.numbersix;
 
 public class Time {
 
+	public Time() {
+
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 31;
+		int prime = 1;
+		result = result * prime;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		if (object == null) {
+			return false;
+		}
+		if (!getClass().equals(object.getClass())) {
+			return false;
+		}
+		Time time = (Time) object;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder build = new StringBuilder();
+		return build.toString();
+	}
 }
