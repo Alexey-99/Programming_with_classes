46f2338fc8b38d01ee2a878d3197002f3c4e3191
Alexey <koroza.alexey99@gmail.com>
2022-05-10 14:23:00 +0200

    *

---
+++ programming_with_classes/src/test/Test.java
@@ -0,0 +1,28 @@
+package test;
+
+import java.util.Arrays;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+	}
+
+	@SuppressWarnings("unused")
+	private static void arrayCopy() {
+		int[] attayOne = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int[] arrayArraysCopyOf = Arrays.copyOf(attayOne, 5);
+		int[] arrayArraysCopyOfRange = Arrays.copyOfRange(attayOne, 1, 6);
+		int[] arraySystemArrayCopy = new int[13];
+		System.arraycopy(attayOne, 2, arraySystemArrayCopy, 5, 7);
+	}
+
+	@SuppressWarnings("unused")
+	private static void creadString() {
+		char[] ch1 = new char[] { 'h', 'e', 'l', 'l', 'o' };
+		char[] ch2 = { 'h', 'e', 'l', 'l', 'o' };
+		String str1 = new String(new char[] { 'h', 'e', 'l', 'l', 'o' });
+		String str2 = new String();
+		String str3 = new String(new char[] { 'w', 'e', 'l', 'c', 'o', 'm', 'e' }, 3, 4);
+	}
+}
