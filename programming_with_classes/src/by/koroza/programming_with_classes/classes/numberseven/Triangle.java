1b9238fa2b1b370e3cd6cb19b47a5f840d093df1
Alexey <koroza.alexey99@gmail.com>
2022-05-17 19:44:09 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/classes/numberseven/Triangle.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/classes/numberseven/Triangle.java
@@ -1,6 +1,38 @@
 package by.koroza.programming_with_classes.classes.numberseven;
 
 public class Triangle {
-	
+	private double sideA;
+	private double sideB;
+	private double sideC;
+
+	public Triangle(double sideA, double sideB, double sideC) { //TODO VALIDATION
+		this.sideA = sideA;
+		this.sideB = sideB;
+		this.sideC = sideC;
+	}
+
+	public double getSideA() {
+		return sideA;
+	}
+
+	public void setSideA(double sideA) {
+		this.sideA = sideA;
+	}
+
+	public double getSideB() {
+		return sideB;
+	}
+
+	public void setSideB(double sideB) {
+		this.sideB = sideB;
+	}
+
+	public double getSideC() {
+		return sideC;
+	}
+
+	public void setSideC(double sideC) {
+		this.sideC = sideC;
+	}
 
 }
