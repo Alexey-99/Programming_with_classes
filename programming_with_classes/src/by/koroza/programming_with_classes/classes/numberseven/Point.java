3c007ee0932a0c45999e3d100f8c72bac551465a
Alexey <koroza.alexey99@gmail.com>
2022-05-18 17:44:02 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/classes/numberseven/Point.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/classes/numberseven/Point.java
@@ -3,9 +3,62 @@ package by.koroza.programming_with_classes.classes.numberseven;
 public class Point {
 	private double x;
 	private double y;
-	
+
 	public Point(double x, double y) {
-		
+		this.x = x;
+		this.y = y;
 	}
 
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 31;
+		int prime = 1;
+		result = result * prime + Double.hashCode(x);
+		result = result * prime + Double.hashCode(y);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		if (object == null) {
+			return false;
+		}
+		if (!getClass().equals(object.getClass())) {
+			return false;
+		}
+		Point point = (Point) object;
+		if (x != point.x) {
+			return false;
+		}
+		if (y != point.y) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("x - ").append(x).append(" y - ").append(y);
+		return builder.toString();
+	}
 }
