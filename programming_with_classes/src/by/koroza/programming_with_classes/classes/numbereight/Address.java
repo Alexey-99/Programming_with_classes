048f324b7be28d6391aabdbf425e5b881bddd721
Alexey <koroza.alexey99@gmail.com>
2022-05-19 21:49:01 +0200

    *

---
+++ programming_with_classes/src/by/koroza/programming_with_classes/classes/numbereight/Address.java
@@ -0,0 +1,122 @@
+package by.koroza.programming_with_classes.classes.numbereight;
+
+public class Address {
+	private String country;
+	private String city;
+	private String street;
+	private int house;
+	private int apartment;
+
+	public Address(String country, String city, String street, int house, int apartment) {
+		this.country = country;
+		this.city = city;
+		this.street = street;
+		this.house = house;
+		this.apartment = apartment;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public int getHouse() {
+		return house;
+	}
+
+	public void setHouse(int house) {
+		this.house = house;
+	}
+
+	public int getApartment() {
+		return apartment;
+	}
+
+	public void setApartment(int apartment) {
+		this.apartment = apartment;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 31;
+		int prime = 1;
+		result = result * prime + (country != null ? country.hashCode() : 1);
+		result = result * prime + (city != null ? city.hashCode() : 1);
+		result = result * prime + (street != null ? street.hashCode() : 1);
+		result = result * prime + house;
+		result = result * prime + apartment;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		if (object == null) {
+			return false;
+		}
+		if (!getClass().equals(object.getClass())) {
+			return false;
+		}
+		Address address = (Address) object;
+		if (country == null) {
+			if (address.country != null) {
+				return false;
+			}
+		} else if (!country.equals(address.country)) {
+			return false;
+		}
+		if (city == null) {
+			if (address.city != null) {
+				return false;
+			}
+		} else if (!city.equals(address.city)) {
+			return false;
+		}
+		if (street == null) {
+			if (address.street != null) {
+				return false;
+			}
+		} else if (!street.equals(address.street)) {
+			return false;
+		}
+		if (house != address.house) {
+			return false;
+		}
+		if (apartment != address.apartment) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Country: ").append(country).append("\n");
+		builder.append("City: ").append(city).append("\n");
+		builder.append("Street: ").append(street).append("\n");
+		builder.append("Number house: ").append(house).append("\n");
+		builder.append("Number apartment: ").append(apartment).append("\n");
+		return builder.toString();
+	}
+}
--- programming_with_classes/src/by/koroza/programming_with_classes/classes/numbereight/Costomer.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/classes/numbereight/Costomer.java
@@ -1,13 +1,162 @@
 package by.koroza.programming_with_classes.classes.numbereight;
 
 public class Costomer {
+	private static int count = 1;
+
 	private int id;
 	private String firstName;
 	private String lastName;
 	private String patronymic;
-	private String address;
+	private Address address;
 	private int creditCardNumber;
 	private int bankAccountNumber;
-	
-	
+
+	public Costomer(String firstName, String lastName, String patronymic, Address address, int creditCardNumber,
+			int bankAccountNumber) {
+		this.id = count++;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.patronymic = patronymic;
+		this.address = address;
+		this.creditCardNumber = creditCardNumber;
+		this.bankAccountNumber = bankAccountNumber;
+	}
+
+	public Costomer(String firstName, String lastName, Address address, int creditCardNumber, int bankAccountNumber) {
+		this.id = count++;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.creditCardNumber = creditCardNumber;
+		this.bankAccountNumber = bankAccountNumber;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPatronymic() {
+		return patronymic;
+	}
+
+	public void setPatronymic(String patronymic) {
+		this.patronymic = patronymic;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public int getCreditCardNumber() {
+		return creditCardNumber;
+	}
+
+	public void setCreditCardNumber(int creditCardNumber) {
+		this.creditCardNumber = creditCardNumber;
+	}
+
+	public int getBankAccountNumber() {
+		return bankAccountNumber;
+	}
+
+	public void setBankAccountNumber(int bankAccountNumber) {
+		this.bankAccountNumber = bankAccountNumber;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 31;
+		int prime = 1;
+		result = result * prime + count;
+		result = result * prime + id;
+		result = result * prime + (firstName != null ? firstName.hashCode() : 1);
+		result = result * prime + (lastName != null ? lastName.hashCode() : 1);
+		result = result * prime + (patronymic != null ? patronymic.hashCode() : 1);
+		result = result * prime + (address != null ? address.hashCode() : 1);
+		result = result * prime + creditCardNumber;
+		result = result * prime + bankAccountNumber;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		if (object == null) {
+			return false;
+		}
+		if (!getClass().equals(object.getClass())) {
+			return false;
+		}
+		Costomer costomer = (Costomer) object;
+		if (id != costomer.id) {
+			return false;
+		}
+		if (firstName == null) {
+			if (costomer.firstName != null) {
+				return false;
+			}
+		} else if (!firstName.equals(costomer.firstName)) {
+			return false;
+		}
+		if (lastName == null) {
+			if (costomer.lastName != null) {
+				return false;
+			}
+		} else if (!lastName.equals(costomer.lastName)) {
+			return false;
+		}
+		if (patronymic == null) {
+			if (patronymic != null) {
+				return false;
+			}
+		} else if (!patronymic.equals(costomer.patronymic)) {
+			return false;
+		}
+		if (address == null) {
+			if (costomer.address != null) {
+				return false;
+			}
+		} else if (!address.equals(costomer.address)) {
+			return false;
+		}
+		if (creditCardNumber != costomer.creditCardNumber) {
+			return false;
+		}
+		if (bankAccountNumber != costomer.bankAccountNumber) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		return builder.toString();
+	}
 }
