2d9b2e094669a8594e654ea6c74f240056bd7d8d
Alexey <koroza.alexey99@gmail.com>
2022-05-20 19:13:46 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/classes/numbereight/CostomerAggregating.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/classes/numbereight/CostomerAggregating.java
@@ -1,5 +1,81 @@
 package by.koroza.programming_with_classes.classes.numbereight;
 
 public class CostomerAggregating {
+	private Costomer[] costomers;
 
+	public CostomerAggregating() {
+
+	}
+
+	public CostomerAggregating(Costomer[] costomers) {
+		this.costomers = costomers;
+	}
+
+	public Costomer[] getCostomers() {
+		return costomers;
+	}
+
+	public void setCostomers(Costomer[] costomers) {
+		this.costomers = costomers;
+	}
+
+	public void addCostomer(Costomer costomer) {
+		Costomer[] costomersNew = new Costomer[costomers.length + 1];
+		for (int i = 0; i < costomersNew.length; i++) {
+			if (i < costomers.length) {
+				costomersNew[i] = costomers[i];
+			} else if (i == costomers.length) {
+				costomersNew[i] = costomer;
+			}
+		}
+		this.costomers = costomersNew;
+	}
+
+	public void addCostomers(Costomer[] costomers) {
+		Costomer[] costomersNew = new Costomer[this.costomers.length + costomers.length];
+		for (int i = 0; i < costomersNew.length; i++) {
+			if (i < this.costomers.length) {
+				costomersNew[i] = this.costomers[i];
+			} else if (i >= costomers.length) {
+				costomersNew[i] = costomers[i - this.costomers.length];
+			}
+		}
+		this.costomers = costomersNew;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 31;
+		int prime = 1;
+		result = result * prime + (costomers != null ? costomers.hashCode() : 1);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		if (object == null) {
+			return false;
+		}
+		if (!getClass().equals(object.getClass())) {
+			return false;
+		}
+		CostomerAggregating aggregating = (CostomerAggregating) object;
+		if (costomers == null) {
+			if (aggregating.costomers != null) {
+				return false;
+			}
+		} else if (!costomers.equals(aggregating.costomers)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		return builder.toString();
+	}
 }
--- programming_with_classes/src/by/koroza/programming_with_classes/classes/numbereight/Main.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/classes/numbereight/Main.java
@@ -13,7 +13,22 @@ package by.koroza.programming_with_classes.classes.numbereight;
 
 public class Main {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
+		Costomer[] costomers = { new Costomer("", "", "", new Address("", "", "", 1, 1), 5, 138),
+				new Costomer("", "", "", new Address("", "", "", 1, 1), 5, 138) };
+		
+		CostomerAggregating costomerAggregating1 = new CostomerAggregating(costomers);
+		CostomerAggregating costomerAggregating2 = new CostomerAggregating();
+
+		costomerAggregating1.addCostomer(new Costomer("Alexey", "Koroza", "Mihaylovich",
+				new Address("Belurus", "Minsk", "asanalieva", 5, 39), 500, 1000));
+		costomerAggregating1.addCostomers(costomers);
+
+		
+		
+		
+		
 		Costomer costomer1 = new Costomer("Alexey", "Koroza", "Mihaylovich",
 				new Address("Belurus", "Minsk", "asanalieva", 5, 39), 500, 1000);
 		System.out.println(costomer1.toString());
