28f72980dfc78a12dd4123c41ad1bfa22d814e02
Alexey <koroza.alexey99@gmail.com>
2022-05-22 16:26:28 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/classes/numbernine/Author.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/classes/numbernine/Author.java
@@ -8,7 +8,94 @@ public class Author {
 	public Author(String lastName, String firstName, String patronymic) {
 		this.lastName = lastName;
 		this.firstName = firstName;
+		this.setPatronymic(patronymic);
+	}
+
+	public Author(String lastName, String firstName) {
+		this.lastName = lastName;
+		this.firstName = firstName;
+	}
+
+	public Author(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getPatronymic() {
+		return patronymic;
+	}
+
+	public void setPatronymic(String patronymic) {
 		this.patronymic = patronymic;
 	}
 
+	@Override
+	public int hashCode() {
+		int result = 31;
+		int prime = 1;
+		result = result * prime + (lastName != null ? lastName.hashCode() : 1);
+		result = result * prime + (firstName != null ? firstName.hashCode() : 1);
+		result = result * prime + (patronymic != null ? patronymic.hashCode() : 1);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		if (object == null) {
+			return false;
+		}
+		if (!getClass().equals(object.getClass())) {
+			return false;
+		}
+		Author author = (Author) object;
+		if (lastName == null) {
+			if (author.lastName != null) {
+				return false;
+			}
+		} else if (!lastName.equals(author.lastName)) {
+			return false;
+		}
+		if (firstName == null) {
+			if (author.firstName != null) {
+				return false;
+			}
+		} else if (!firstName.equals(author.firstName)) {
+			return false;
+		}
+		if (patronymic == null) {
+			if (author.patronymic != null) {
+				return false;
+			}
+		} else if (!patronymic.equals(author.patronymic)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Lastname: ").append(lastName).append("\n");
+		builder.append("Firstname: ").append(firstName).append("\n");
+		builder.append("Patronymic: ").append(patronymic).append("\n");
+		return builder.toString();
+	}
 }
