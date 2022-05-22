96ee296cd6c706940ca30a11b79d8437b114abcf
Alexey <koroza.alexey99@gmail.com>
2022-05-22 21:40:55 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberone/Sentence.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberone/Sentence.java
@@ -3,4 +3,10 @@ package by.koroza.programming_with_classes.composition.numberone;
 public class Sentence {
 	private Word[] words;
 
+	public Sentence(Word[] words) {
+		this.words = words;
+	}
+	
+	
+
 }
--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberone/Word.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberone/Word.java
@@ -1,5 +1,54 @@
 package by.koroza.programming_with_classes.composition.numberone;
 
 public class Word {
+	private String word;
 
+	public Word(String word) {
+		this.word = word;
+	}
+
+	public String getWord() {
+		return word;
+	}
+
+	public void setWord(String word) {
+		this.word = word;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 31;
+		int prime = 1;
+		result = result * prime + (word != null ? word.hashCode() : 1);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		if (object == null) {
+			return false;
+		}
+		if (!getClass().equals(object.getClass())) {
+			return false;
+		}
+		Word word = (Word) object;
+		if (this.word == null) {
+			if (word.word != null) {
+				return false;
+			}
+		} else if (!this.word.equals(word.word)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(word);
+		return builder.toString();
+	}
 }
