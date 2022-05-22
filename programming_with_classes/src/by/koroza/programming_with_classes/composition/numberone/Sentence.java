16234f7bdaf0b6051f333afa950cc8de59f6a0db
Alexey <koroza.alexey99@gmail.com>
2022-05-22 21:45:14 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberone/Sentence.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberone/Sentence.java
@@ -3,10 +3,57 @@ package by.koroza.programming_with_classes.composition.numberone;
 public class Sentence {
 	private Word[] words;
 
+	public Sentence() {
+
+	}
+
 	public Sentence(Word[] words) {
 		this.words = words;
 	}
-	
-	
+
+	public Word[] getWords() {
+		return words;
+	}
+
+	public void setWords(Word[] words) {
+		this.words = words;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 31;
+		int prime = 1;
+		result = result * prime + (words != null ? words.hashCode() : 1);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		if (object == null) {
+			return false;
+		}
+		if (!getClass().equals(object.getClass())) {
+			return false;
+		}
+		Sentence sentence = (Sentence) object;
+		if (this.words == null) {
+			if (sentence.words != null) {
+				return false;
+			}
+		} else if (!words.equals(sentence.words)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(words);
+		return builder.toString();
+	}
 
 }
