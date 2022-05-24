d0664830ce8a9843e33b548965bda512c0656cd6
Alexey <koroza.alexey99@gmail.com>
2022-05-24 20:01:19 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numbertwo/Engine.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numbertwo/Engine.java
@@ -2,4 +2,33 @@ package by.koroza.programming_with_classes.composition.numbertwo;
 
 public class Engine {
 
+	@Override
+	public int hashCode() {
+		int result = 31;
+		int prime = 1;
+		result = result * prime;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		if (object == null) {
+			return false;
+		}
+		if (!getClass().equals(object.getClass())) {
+			return false;
+		}
+		Engine engine = (Engine) object;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		return builder.toString();
+	}
+
 }
