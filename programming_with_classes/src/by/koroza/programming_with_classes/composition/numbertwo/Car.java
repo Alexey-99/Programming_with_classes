d1c6523d1ca52498bb36165c8afaad7a3a61aba5
Alexey <koroza.alexey99@gmail.com>
2022-05-24 20:13:09 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numbertwo/Car.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numbertwo/Car.java
@@ -1,12 +1,20 @@
 package by.koroza.programming_with_classes.composition.numbertwo;
 
 public class Car {
+	private Wheel[] wheels = new Wheel[4];
+	private Engine engine;
+
+	public Car(Wheel[] wheels, Engine engine) {
+		this.wheels = wheels;
+		this.engine = engine;
+	}
 
 	@Override
 	public int hashCode() {
 		int result = 31;
 		int prime = 1;
-		result = result * prime;
+		result = result * prime + (wheels != null ? wheels.hashCode() : 1);
+		result = result * prime + (engine != null ? engine.hashCode() : 1);
 		return result;
 	}
 
@@ -22,6 +30,20 @@ public class Car {
 			return false;
 		}
 		Car car = (Car) object;
+		if (wheels == null) {
+			if (car.wheels != null) {
+				return false;
+			}
+		} else if (!wheels.equals(car.wheels)) {
+			return false;
+		}
+		if (engine == null) {
+			if (car.engine != null) {
+				return false;
+			}
+		} else if (!engine.equals(car.engine)) {
+			return false;
+		}
 		return true;
 	}
 
@@ -30,5 +52,4 @@ public class Car {
 		StringBuilder builder = new StringBuilder();
 		return builder.toString();
 	}
-
 }
