8d69dd7c2c44e7a20a38c206c1b93f026bf4ddf7
Alexey <koroza.alexey99@gmail.com>
2022-05-30 14:24:54 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numbertwo/Engine.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numbertwo/Engine.java
@@ -1,13 +1,29 @@
 package by.koroza.programming_with_classes.composition.numbertwo;
 
 public class Engine {
-	private double fuelVolume; 
+	private double wear;
+
+	public Engine() {
+		this.wear = 0;
+	}
+
+	public Engine(double wear) {
+		this.wear = wear;
+	}
+
+	public double getWear() {
+		return wear;
+	}
+
+	public void setWear(double wear) {
+		this.wear = wear;
+	}
 
 	@Override
 	public int hashCode() {
 		int result = 31;
 		int prime = 1;
-		result = result * prime;
+		result = result * prime + Double.hashCode(wear);
 		return result;
 	}
 
@@ -23,13 +39,16 @@ public class Engine {
 			return false;
 		}
 		Engine engine = (Engine) object;
+		if (wear != engine.wear) {
+			return false;
+		}
 		return true;
 	}
 
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
+		builder.append("Engine wear: ").append(false).append("%");
 		return builder.toString();
 	}
-
 }
