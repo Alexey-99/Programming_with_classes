79757d9f6e72c4d84d3b868bdf101a4145b07cfd
Alexey <koroza.alexey99@gmail.com>
2022-05-31 20:47:18 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberthree/Region.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberthree/Region.java
@@ -2,4 +2,32 @@ package by.koroza.programming_with_classes.composition.numberthree;
 
 public class Region {
 
+	@Override
+	public int hashCode() {
+		int result = 31;
+		int prime = 1;
+		result = result * prime;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		if (object == null) {
+			return false;
+		}
+		if (!getClass().equals(object.getClass())) {
+			return false;
+		}
+		Region region = (Region) object;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		return builder.toString();
+	}
 }
