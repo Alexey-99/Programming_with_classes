1119f3721b92989479b500602a24e289615fae3c
Alexey <koroza.alexey99@gmail.com>
2022-06-01 17:01:21 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberthree/City.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberthree/City.java
@@ -2,10 +2,38 @@ package by.koroza.programming_with_classes.composition.numberthree;
 
 public class City {
 	private String name;
+	private String cityStatus;
 	private District[] districts;
 
 	public City(String name) {
 		this.name = name;
+	}
+
+	public City(String name, String cityStatus) {
+		this.name = name;
+		this.cityStatus = cityStatus;
+	}
+
+	public City(String name, District[] districts) {
+		this.name = name;
+		this.districts = districts;
+	}
+
+	public City(String name, District[] districts, String cityStatus) {
+		this.name = name;
+		this.districts = districts;
+		this.cityStatus = cityStatus;
+	}
+
+	public City(String name, District district) {
+		this.name = name;
+		this.districts[0] = district;
+	}
+
+	public City(String name, District district, String cityStatus) {
+		this.name = name;
+		this.districts[0] = district;
+		this.cityStatus = cityStatus;
 	}
 
 	public String getName() {
@@ -24,10 +52,19 @@ public class City {
 		this.districts = districts;
 	}
 
+	public String getCityStatus() {
+		return cityStatus;
+	}
+
+	public void setCityStatus(String cityStatus) {
+		this.cityStatus = cityStatus;
+	}
+
 	@Override
 	public int hashCode() {
 		int result = 31;
 		int prime = 1;
+		result = result * prime + (cityStatus != null ? cityStatus.hashCode() : 1);
 		result = result * prime + (name != null ? name.hashCode() : 1);
 		result = result * prime + (districts != null ? districts.hashCode() : 1);
 		return result;
@@ -45,6 +82,13 @@ public class City {
 			return false;
 		}
 		City city = (City) object;
+		if (cityStatus == null) {
+			if (city.cityStatus != null) {
+				return false;
+			}
+		} else if (!cityStatus.equals(city.cityStatus)) {
+			return false;
+		}
 		if (name == null) {
 			if (city.name != null) {
 				return false;
