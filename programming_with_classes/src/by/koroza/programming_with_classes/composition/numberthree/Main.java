67c74ace3555caa931cf090f023f7a4c621b6636
Alexey <koroza.alexey99@gmail.com>
2022-06-01 17:26:37 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberthree/Main.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberthree/Main.java
@@ -1,12 +1,16 @@
 /* Создать объект класса Государство, используя классы Область, Район, Город. 
  * Методы: вывести на консоль столицу, количество областей, площадь, областные центры. 
- */  
+ */
 
 package by.koroza.programming_with_classes.composition.numberthree;
 
 public class Main {
 
 	public static void main(String[] args) {
-		State state = new State("USA");
+		City[] minsk = {new City("Minsk")};
+		
+		Region[] regions = { new Region("Minsk"), new Region("Brest"), new Region("Grodno"), new Region("Vitebsk"),
+				new Region("Mogilev"), new Region("Gomel") };
+		State state = new State("USA", regions);
 	}
 }
