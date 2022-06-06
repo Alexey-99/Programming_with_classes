977266e2f945a2331dd57fea7ead37c835bdfb1d
Alexey <koroza.alexey99@gmail.com>
2022-06-06 18:23:33 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfour/Person.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfour/Person.java
@@ -1,5 +1,17 @@
 package by.koroza.programming_with_classes.composition.numberfour;
 
 public class Person {
+	private int id;
+	private String lastName;
+	private String firstName;
+	private String patronymic;
+	private BankAccount[] bankAccounts;
 
+	public BankAccount[] getBankAccounts() {
+		return bankAccounts;
+	}
+
+	public void setBankAccounts(BankAccount[] bankAccounts) {
+		this.bankAccounts = bankAccounts;
+	}
 }
