9140819a2208d724b09a792d3a521d2679b07bfc
Alexey <koroza.alexey99@gmail.com>
2022-06-06 19:57:06 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfour/BankAccount.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfour/BankAccount.java
@@ -6,10 +6,12 @@ public class BankAccount {
 	private int number;
 	private double sum;
 	private Operation[] operations;
+	private String status;
 
 	public BankAccount() {
 		this.number = count++;
 		this.sum = 0;
+		this.status = "Active";
 	}
 
 	public int getNumber() {
@@ -36,6 +38,14 @@ public class BankAccount {
 		this.operations = operations;
 	}
 
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
 	public void addOperation(Operation operation) {
 		if (this.operations == null) {
 			this.operations[0] = operation;
@@ -52,6 +62,59 @@ public class BankAccount {
 				this.operations[this.operations.length + i] = operations[i];
 			}
 		}
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 31;
+		int prime = 1;
+		result = result * prime + count;
+		result = result * prime + number;
+		result = result * prime + (Double.hashCode(sum));
+		result = result * prime + (operations != null ? operations.hashCode() : 1);
+		result = result * prime + (status != null ? status.hashCode() : 1);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object) {
+			return true;
+		}
+		if (object == null) {
+			return false;
+		}
+		if (!getClass().equals(object.getClass())) {
+			return false;
+		}
+		BankAccount account = (BankAccount) object;
+		if (number != account.number) {
+			return false;
+		}
+		if (sum != account.sum) {
+			return false;
+		}
+		if (operations == null) {
+			if (account.operations != null) {
+				return false;
+			}
+		} else if (!operations.equals(account.operations)) {
+			return false;
+		}
+		if (status == null) {
+			if (account.status != null) {
+				return false;
+			}
+		} else if (!status.equals(account.status)) {
+			return false;
+		}
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		return builder.toString();
 	}
 
 }
--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfour/Operation.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfour/Operation.java
@@ -1,5 +1,39 @@
 package by.koroza.programming_with_classes.composition.numberfour;
 
-public class Operation {
+import java.util.Date;
 
+public class Operation {
+	private Date date;
+	private double sum;
+	private String comment;
+	
+	public Operation(double sum, String comment) {
+		//this.date;
+		this.sum = sum;
+		this.comment = comment;
+	}
+
+	public double getSum() {
+		return sum;
+	}
+
+	public void setSum(double sum) {
+		this.sum = sum;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
 }
