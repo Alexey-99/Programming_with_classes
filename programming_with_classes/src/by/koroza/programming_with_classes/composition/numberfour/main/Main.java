222dc39116ae256734152b38118285b08299f7ab
Alexey <koroza.alexey99@gmail.com>
2022-06-08 18:13:31 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfour/main/Main.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfour/main/Main.java
@@ -8,6 +8,11 @@
 package by.koroza.programming_with_classes.composition.numberfour.main;
 
 public class Main {
+	static final String SEARCH_BANK_ACCOUNTS_WITH_BALANCE_RANGE = "Search for bank accounts with a balance in the range:";
+	static final String SEARCH_BANK_ACCOUNTS_WITH_SUM_BALANCE = "Search for bank accounts with sum balance:";
+	static final String SEARCH_BANK_ACCOUNTS_WITH_NUMBER_BANK_ACCOUNT_RANGE = "Search for bank accounts with a number bank account in the range:";
+	static final String SEARCH_BANK_ACCOUNTS_WITH_NUMBER_BANK_ACCOUNT = "Search for bank accounts with number bank account:";
+	static final String SEARCH_BANK_ACCOUNTS_WITH_STATUS_BANK_ACCOUNT = "Search for bank accounts with staus bank account:";
 
 	public static void main(String[] args) {
 		Person person = new Person("Koroza", "Alexey", "M", new BankAccount());
@@ -28,31 +33,31 @@ public class Main {
 	}
 
 	private static void search(Person person) {
-		System.out.println("\n" + "Search for bank accounts with a balance in the range:");
+		System.out.println("\n" + SEARCH_BANK_ACCOUNTS_WITH_BALANCE_RANGE);
 		person.searchByMinToMaxBalance(1, 10);
 
-		System.out.println("\n" + "Search for bank accounts with a balance in the range:");
+		System.out.println("\n" + SEARCH_BANK_ACCOUNTS_WITH_BALANCE_RANGE);
 		person.searchByMinBalance(0);
 
-		System.out.println("\n" + "Search for bank accounts with a balance in the range:");
+		System.out.println("\n" + SEARCH_BANK_ACCOUNTS_WITH_BALANCE_RANGE);
 		person.searchByMaxBalance(10);
 
-		System.out.println("\n" + "Search for bank accounts with sum balance:");
+		System.out.println("\n" + SEARCH_BANK_ACCOUNTS_WITH_SUM_BALANCE);
 		person.searchByBalance(0);
 
-		System.out.println("\n" + "Search for bank accounts with a number bank account in the range:");
+		System.out.println("\n" + SEARCH_BANK_ACCOUNTS_WITH_NUMBER_BANK_ACCOUNT_RANGE);
 		person.searchByMinToMaxNumber(1, 3);
 
-		System.out.println("\n" + "Search for bank accounts with a number bank account in the range:");
+		System.out.println("\n" + SEARCH_BANK_ACCOUNTS_WITH_NUMBER_BANK_ACCOUNT_RANGE);
 		person.searchByMinNumber(3);
 
-		System.out.println("\n" + "Search for bank accounts with a number bank account in the range:");
+		System.out.println("\n" + SEARCH_BANK_ACCOUNTS_WITH_NUMBER_BANK_ACCOUNT_RANGE);
 		person.searchByMaxNumber(5);
 
-		System.out.println("\n" + "Search for bank accounts with number bank account:");
+		System.out.println("\n" + SEARCH_BANK_ACCOUNTS_WITH_NUMBER_BANK_ACCOUNT);
 		person.searchByNumber(1);
 
-		System.out.println("\n" + "Search for bank accounts with staus bank account:");
+		System.out.println("\n" + SEARCH_BANK_ACCOUNTS_WITH_STATUS_BANK_ACCOUNT);
 		person.searchByStatus("active");
 	}
 
