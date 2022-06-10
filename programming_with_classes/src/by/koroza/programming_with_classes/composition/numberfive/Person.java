f1fcaeef492d78921aa7577b37a489adb422f334
Alexey <koroza.alexey99@gmail.com>
2022-06-10 20:19:52 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfive/Person.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfive/Person.java
@@ -4,7 +4,102 @@ public class Person {
 	private String lastName;
 	private String firstName;
 	private String patronymic;
-	private TravelVoucher[] travelVoucher;
-	
-	
+	private TravelVoucher[] travelVouchers;
+
+	public Person(String lastName, String firstName, String patronymic) {
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.patronymic = patronymic;
+		this.travelVouchers = new TravelVoucher[1];
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getPatronymic() {
+		return patronymic;
+	}
+
+	public void setPatronymic(String patronymic) {
+		this.patronymic = patronymic;
+	}
+
+	public TravelVoucher[] getTravelVouchers() {
+		return travelVouchers;
+	}
+
+	public void setTravelVouchers(TravelVoucher[] travelVouchers) {
+		this.travelVouchers = travelVouchers;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = 31;
+		int prime = 1;
+		result = prime * result + (lastName != null ? lastName.hashCode() : 1);
+		result = prime * result + (firstName != null ? firstName.hashCode() : 1);
+		result = prime * result + (patronymic != null ? patronymic.hashCode() : 1);
+		result = prime * result + (travelVouchers != null ? travelVouchers.hashCode() : 1);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object object) {
+		if (this == object)
+			return true;
+		if (object == null)
+			return false;
+		if (!getClass().equals(object.getClass())) {
+			return false;
+		}
+		Person person = (Person) object;
+		if (lastName == null) {
+			if (person.lastName != null) {
+				return false;
+			}
+		} else if (!lastName.equals(person.lastName)) {
+			return false;
+		}
+		if (firstName == null) {
+			if (person.firstName != null) {
+				return false;
+			}
+		} else if (!firstName.equals(person.firstName)) {
+			return false;
+		}
+		if (patronymic == null) {
+			if (person.patronymic != null) {
+				return false;
+			}
+		} else if (!patronymic.equals(person.patronymic)) {
+			return false;
+		}
+		if (travelVouchers == null) {
+			if (person.travelVouchers != null) {
+				return false;
+			}
+		} else if (!travelVouchers.equals(person.travelVouchers)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		return builder.toString();
+	}
 }
