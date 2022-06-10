230680cacef2b0abc995fa976cc5b41ca936638a
Alexey <koroza.alexey99@gmail.com>
2022-06-10 19:59:09 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfive/Person.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfive/Person.java
@@ -1,5 +1,10 @@
 package by.koroza.programming_with_classes.composition.numberfive;
 
 public class Person {
-
+	private String lastName;
+	private String firstName;
+	private String patronymic;
+	private TravelVoucher[] travelVoucher;
+	
+	
 }
