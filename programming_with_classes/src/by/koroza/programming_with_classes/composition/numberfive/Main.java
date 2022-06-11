aa49ba0033c6ddd14289d71c769f6fcd7e7a512a
Alexey <koroza.alexey99@gmail.com>
2022-06-11 20:24:36 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfive/Main.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfive/Main.java
@@ -10,6 +10,39 @@ package by.koroza.programming_with_classes.composition.numberfive;
 public class Main {
 
 	public static void main(String[] args) {
-		TravelCampany campany = new TravelCampany("AlexKor");
+		TravelVoucher[] travelVouchers = { new TravelVoucher("Turkey", "Alanya", "beach tour", 11, 865),
+				new TravelVoucher("Egypt", "Sharm El Sheikh", "beach tour", 11, 880),
+				new TravelVoucher("Russia", "St. Petersburg", "excursion tour", 4, 130),
+				new TravelVoucher("Italy", "San Marino", "sea + excursion tour", 10, 330,
+						"Italian holidays without night crossings 3 places for tourists with visas (Italy, Austria, Czech Republic)"),
+				new TravelVoucher("Belarus", "children's rest", 14, 374,
+						"Children's health resort NAROCHKIDS in Naroch resort"),
+				new TravelVoucher("Georgia", "Tbilisi", "sea + excursion tour", 15, 210),
+				new TravelVoucher("Georgia", "Kobuleti", "sea + excursion tour", 15, 290,
+						"To Georgia by sea - Kobuleti"),
+				new TravelVoucher("Hungary", "Slovakia", "excursion tour", 4, 179,
+						"Hungary is a thermal paradise! For tourists with a Visa!"),
+				new TravelVoucher("Россия", "St. Petersburg", "excursion tour", 4, 95,
+						"White Nights and Bright Weekends in St. Petersburg! All 4 excursions are already included in the tour price "),
+				new TravelVoucher("Turkey", "Istanbul", "excursion tour", 7, 610,
+						"Istanbul with departure from Minsk!"),
+				new TravelVoucher("Russia", "St. Petersburg", "excursion tour", 4, 95,
+						"Bright weekend in St. Petersburg! All 4 excursions are already included in the tour price"),
+				new TravelVoucher("Russia", "St. Petersburg", "excursion tour", 3, 85,
+						"Myths and legends of St. Petersburg (2 days)"),
+				new TravelVoucher("Russia", "St. Petersburg", "excursion tour", 4, 135,
+						"St. Petersburg-Peterhof-Kronstadt-Tsarskoye Selo"),
+				new TravelVoucher("Russia", "excursion tour", 4, 105, "gold ring of Russia "),
+				new TravelVoucher("Russia", "St. Petersburg", "excursion tour", 4, 100,
+						"Petersburg + Karelia! Karelia Free!!! No hidden extras! All sightseeing tours are included"),
+				new TravelVoucher("Russia", "St. Petersburg", "excursion tour", 4, 95,
+						"Bright weekend in St. Petersburg! All 4 excursions are already included in the tour price "),
+				new TravelVoucher("Russia", "St. Petersburg", "excursion tour", 4, 100,
+						"Petersburg + Karelia! Karelia Free!!! No hidden extras! All sightseeing tours are included"),
+				new TravelVoucher("Turkey", "Istanbul", "excursion tour", 5, 545,
+						"Avia. Spring in Istanbul. Tours you choose"),
+				new TravelVoucher("Russia", "Moskow", "excursion tour", 4, 105, "Bus tour " + "'Moscow of many faces'"),
+				new TravelVoucher("Turkey", "Kemer", "beach tour", 11, 1035, "Tour to Larissa Sultan`s Beach Hotel") };
+		TravelCampany campany = new TravelCampany("AlexKor", travelVouchers);
 	}
 }
--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfive/TravelVoucher.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfive/TravelVoucher.java
@@ -1,6 +1,85 @@
 package by.koroza.programming_with_classes.composition.numberfive;
 
 public class TravelVoucher {
+	private String country;
+	private String city;
+	private String tourType;
+	private int numberNights;
+	private double price;
+	private String comment;
+
+	public TravelVoucher(String country, String tourType, int numberNights, double price, String comment) {
+		this.country = country;
+		this.tourType = tourType;
+		this.numberNights = numberNights;
+		this.price = price;
+		this.comment = comment;
+	}
+
+	public TravelVoucher(String country, String city, String tourType, int numberNights, double price) {
+		this.country = country;
+		this.city = city;
+		this.tourType = tourType;
+		this.numberNights = numberNights;
+		this.price = price;
+	}
+	
+	public TravelVoucher(String country, String city, String tourType, int numberNights, double price, String comment) {
+		this.country = country;
+		this.city = city;
+		this.tourType = tourType;
+		this.numberNights = numberNights;
+		this.price = price;
+		this.comment = comment;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getTourType() {
+		return tourType;
+	}
+
+	public void setTourType(String tourType) {
+		this.tourType = tourType;
+	}
+
+	public int getNumberNights() {
+		return numberNights;
+	}
+
+	public void setNumberNights(int numberNights) {
+		this.numberNights = numberNights;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
 
 	@Override
 	public int hashCode() {
