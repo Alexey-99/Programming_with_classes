4ae7781b43509b75c1ea248ddd69e4215bb9d9fa
Alexey <koroza.alexey99@gmail.com>
2022-06-12 18:50:25 +0200

    *

---
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfive/enums/CityEnumeration.java
@@ -0,0 +1,14 @@
+package by.koroza.programming_with_classes.composition.numberfive.enums;
+
+public class CityEnumeration {
+	private static String[] CITIES = { "Alanya", "Sharm El Sheikh", "St. Petersburg", "San Marino", "Tbilisi",
+			"Kobuleti", "Slovakia", "Istanbul", "Moskow", "Kemer" };
+
+	public static String[] getCITIES() {
+		return CITIES;
+	}
+	
+	public static void addCity() {
+		
+	}
+}
