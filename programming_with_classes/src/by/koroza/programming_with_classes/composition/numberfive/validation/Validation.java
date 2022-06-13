f035c1994788880d666c40525fd53d75b294ad0d
Alexey <koroza.alexey99@gmail.com>
2022-06-13 12:23:54 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfive/validation/Validation.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfive/validation/Validation.java
@@ -1,6 +1,7 @@
 package by.koroza.programming_with_classes.composition.numberfive.validation;
 
 import static by.koroza.programming_with_classes.composition.numberfive.enums.CountryEnumeration.getCountries;
+import static by.koroza.programming_with_classes.composition.numberfive.enums.CityEnumeration.getCities;
 
 public class Validation {
 
@@ -13,9 +14,14 @@ public class Validation {
 		}
 		return isHavingCountry;
 	}
-	
+
 	public static boolean checkingOnHavingCityInList(String city) {
 		boolean isHavingCity = false;
+		for (String cityElement : getCities()) {
+			if (cityElement.equals(city)) {
+				isHavingCity = true;
+			}
+		}
 		return isHavingCity;
 	}
 }
