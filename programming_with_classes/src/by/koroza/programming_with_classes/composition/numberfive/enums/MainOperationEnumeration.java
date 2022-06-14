ee29e69394e68473968e8b630fe700399607258b
Alexey <koroza.alexey99@gmail.com>
2022-06-14 12:33:25 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfive/enums/MainOperationEnumeration.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfive/enums/MainOperationEnumeration.java
@@ -2,7 +2,7 @@ package by.koroza.programming_with_classes.composition.numberfive.enums;
 
 public class MainOperationEnumeration {
 	private static String[] operations = { "View all travel packages", "View countries that have travel packages",
-			"View cities that have tourist packages",  };
+			"View cities that have tourist packages", "View types of travel packages" };
 
 	public static String[] getOperations() {
 		return operations;
@@ -11,6 +11,15 @@ public class MainOperationEnumeration {
 	public static void setOperations(String[] operations) {
 		MainOperationEnumeration.operations = operations;
 	}
-	
-	
+
+	public static void printOperations() {
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < operations.length; i++) {
+			builder.append(i).append(" - ").append(operations[i]);
+			if (i < operations.length - 1) {
+				builder.append("\n");
+			}
+		}
+		System.out.println(builder);
+	}
 }
--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfive/programprocess/ChoiceMainAction.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberfive/programprocess/ChoiceMainAction.java
@@ -1,10 +1,11 @@
 package by.koroza.programming_with_classes.composition.numberfive.programprocess;
 
+import by.koroza.programming_with_classes.composition.numberfive.enums.MainOperationEnumeration;
+
 public class ChoiceMainAction {
 	
 	public static void choiceMainAction() {
 		System.out.println("Enter number operation");
-		
+		MainOperationEnumeration.printOperations();
 	}
-
 }
