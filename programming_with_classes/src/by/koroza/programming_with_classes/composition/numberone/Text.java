dce6fa4ce799eb8ff2b4b0d38f389d8a14c85a95
Alexey <koroza.alexey99@gmail.com>
2022-06-21 21:40:17 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/composition/numberone/Text.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/composition/numberone/Text.java
@@ -1,11 +1,14 @@
 package by.koroza.programming_with_classes.composition.numberone;
 
 public class Text {
+	private static final String SPACE = " ";
+	private static final String NEXT_LINE = "\n";
+	
 	private String title;
 	private Sentence[] sentences;
 
 	public Text() {
-
+		this.sentences = new Sentence[0];
 	}
 
 	public Text(String title, Sentence[] sentences) {
@@ -30,37 +33,27 @@ public class Text {
 	}
 
 	public void addSentence(Sentence sentence) {
-		Sentence[] sentencesNew = null;
-		if (sentences == null) {
-			sentences[0] = sentence;
-		} else {
-			sentencesNew = new Sentence[sentences.length + 1];
-			for (int i = 0; i < sentencesNew.length; i++) {
-				if (i < sentences.length) {
-					sentencesNew[i] = sentences[i];
-				} else if (i == sentences.length) {
-					sentencesNew[i] = sentence;
-				}
+		Sentence[] sentencesNew = new Sentence[sentences.length + 1];
+		for (int i = 0; i < sentencesNew.length; i++) {
+			if (i < sentences.length) {
+				sentencesNew[i] = sentences[i];
+			} else if (i == sentences.length) {
+				sentencesNew[i] = sentence;
 			}
-			sentences = sentencesNew;
 		}
+		sentences = sentencesNew;
 	}
 
 	public void addSentences(Sentence[] sentences) {
-		Sentence[] sentencesNew = null;
-		if (this.sentences == null) {
-			this.sentences = sentences;
-		} else {
-			sentencesNew = new Sentence[this.sentences.length + sentences.length];
-			for (int i = 0; i < sentencesNew.length; i++) {
-				if (i < this.sentences.length) {
-					sentencesNew[i] = this.sentences[i];
-				} else if (i >= this.sentences.length) {
-					sentencesNew[i] = sentences[i - this.sentences.length];
-				}
+		Sentence[] sentencesNew = new Sentence[this.sentences.length + sentences.length];
+		for (int i = 0; i < sentencesNew.length; i++) {
+			if (i < this.sentences.length) {
+				sentencesNew[i] = this.sentences[i];
+			} else if (i >= this.sentences.length) {
+				sentencesNew[i] = sentences[i - this.sentences.length];
 			}
-			this.sentences = sentencesNew;
 		}
+		this.sentences = sentencesNew;
 	}
 
 	@Override
@@ -69,6 +62,8 @@ public class Text {
 		int prime = 1;
 		result = result * prime + (title != null ? title.hashCode() : 1);
 		result = result * prime + (sentences != null ? sentences.hashCode() : 1);
+		result = result * prime + (SPACE != null ? SPACE.hashCode() : 1);
+		result = result * prime + (NEXT_LINE != null ? NEXT_LINE.hashCode() : 1);
 		return result;
 	}
 
@@ -105,11 +100,11 @@ public class Text {
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append(title).append("\n");
+		builder.append(title).append(NEXT_LINE);
 		for (int i = 0; i < sentences.length; i++) {
 			builder.append(sentences[i].toString());
 			if (i < sentences.length - 1) {
-				builder.append(" ");
+				builder.append(SPACE);
 			}
 		}
 		return builder.toString();
