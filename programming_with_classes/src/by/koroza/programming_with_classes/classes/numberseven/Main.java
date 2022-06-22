1d644150e24a983d60b44de7bcb2640b7aa3b409
Alexey <koroza.alexey99@gmail.com>
2022-06-22 16:08:09 +0200

    *

--- programming_with_classes/src/by/koroza/programming_with_classes/classes/numberseven/Main.java
+++ programming_with_classes/src/by/koroza/programming_with_classes/classes/numberseven/Main.java
@@ -6,29 +6,35 @@
 package by.koroza.programming_with_classes.classes.numberseven;
 
 public class Main {
+	static final String CREATE_TRIANGLE_EMPTY_CONSTRUCTOR = "Create triangle with empty constructor";
+	static final String CREATE_TRIANGLE_CONSTRUCTOR_SIDES_AND_POINT_OBJECTS = "Create triangle with constructor with sides and coordinates (enter objects)";
+	static final String CREATE_TRIANGLE_CONSTRUCTOR_SIDES_AND_POINT_NUMBERS = "Create triangle with constructor with sides and coordinates (enter number)";
+	static final String CREATE_TRIANGLE_CONSTRUCTOR_SIDES = "Create triangle with constructor with sides";
+	static final String CREATE_TRIANGLE_CONSTRUCTOR_POINT_OBJECTS = "Create triangle with constructor with coordinates (enter objects)";
+	static final String CREATE_TRIANGLE_CONSTRUCTOR_POINT_NUMBERS = "Create triangle with constructor with coordinates (enter number)";
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("Create triangle with empty constructor");
+		System.out.println(CREATE_TRIANGLE_EMPTY_CONSTRUCTOR);
 		Triangle triangle1 = new Triangle();
 		System.out.println(triangle1.toString());
 
-		System.out.println("Create triangle with constructor with sides and coordinates (enter objects)");
+		System.out.println(CREATE_TRIANGLE_CONSTRUCTOR_SIDES_AND_POINT_OBJECTS);
 		Triangle triangle2 = new Triangle(1, 2, 2, new Point(1, 1), new Point(2, 2), new Point(1, 3));
 		System.out.println(triangle2.toString());
 
-		System.out.println("Create triangle with constructor with sides and coordinates (enter number)");
+		System.out.println(CREATE_TRIANGLE_CONSTRUCTOR_SIDES_AND_POINT_NUMBERS);
 		Triangle triangle3 = new Triangle(1, 2, 2, 1, 1, 2, 2, 1, 3);
 		System.out.println(triangle3.toString());
 
-		System.out.println("Create triangle with constructor with sides");
+		System.out.println(CREATE_TRIANGLE_CONSTRUCTOR_SIDES);
 		Triangle triangle4 = new Triangle(1, 2, 2);
 		System.out.println(triangle4.toString());
 
-		System.out.println("Create triangle with constructor with coordinates (enter objects)");
+		System.out.println(CREATE_TRIANGLE_CONSTRUCTOR_POINT_OBJECTS);
 		Triangle triangle5 = new Triangle(new Point(1, 1), new Point(2, 2), new Point(1, 3));
 		System.out.println(triangle5.toString());
 
-		System.out.println("Create triangle with constructor with coordinates (enter number)");
+		System.out.println(CREATE_TRIANGLE_CONSTRUCTOR_POINT_NUMBERS);
 		Triangle triangle6 = new Triangle(1, 1, 2, 2, 1, 3);
 		System.out.println(triangle6.toString());
 
